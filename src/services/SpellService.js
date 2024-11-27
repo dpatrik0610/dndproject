8b363ff396baea918858084a290992dfa4c3f0b7
Patrik Dobos <d.patrik0610@gmail.com>
2024-11-28 00:09:25 +0100

    created SpellService.js

--- src/services/SpellService.js
+++ src/services/SpellService.js
@@ -1,16 +1,45 @@
 class SpellService {
-    constructor(spellRepository) {
-      this.spellRepository = spellRepository;
-    }
+  constructor(spellRepository, dnd5eClient) {
+    this.spellRepository = spellRepository;
+    this.dnd5eClient = dnd5eClient;
+
+    this.originalSpells = this.dnd5eClient.getSpells();  // Ensure this only happens once
+  }
   
-    async getSpells() {
-      return this.spellRepository.getAll();
-    }
+  getOriginalSpells = () => {
+    return this.originalSpells;
+  }
   
-    async createSpell(spellData) {
-      return this.spellRepository.create(spellData);
+  getCustomSpells = async () => {
+    try {
+      return await this.spellRepository.getAll();
+    } catch (err) {
+      console.error("Couldn't get custom spells from repository.");
+      throw new Error("Couldn't get custom spells from repository.");
     }
   }
   
-  module.exports = SpellService;
+  // Returns both custom and original spells
+  async getAllSpells() {
+    const originalSpells = this.getOriginalSpells();
+    const customSpells = await this.getCustomSpells();
+    return [...originalSpells, ...customSpells];
+  }
   
+  // Finds a spell first in custom list, then in original list
+  async findSpell(spellIndex) {
+    let spell = await this.spellRepository.getByIndex(spellIndex);
+    
+    if (!spell) {
+      spell = this.originalSpells.find(spell => spell.index.toLowerCase() === spellIndex.toLowerCase());
+    }
+
+    if (!spell) {
+      throw new Error(`Spell with index ${spellIndex} not found.`);
+    }
+
+    return spell;
+  }
+}
+
+module.exports = SpellService;
