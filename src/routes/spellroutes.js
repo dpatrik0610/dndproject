1226ad7c0ba579de29dbcb415cbbf1233634bfa2
Patrik Dobos <d.patrik0610@gmail.com>
2024-11-28 00:31:05 +0100

    Added SpellRoutes.js

--- src/routes/spellroutes.js
+++ src/routes/spellroutes.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const axios = require('axios');
+const Dnd5eClient = require('../repositories/Dnd5eClient');
+const SpellRepository = require("../repositories/SpellRepository");
+const SpellService = require('../services/SpellService');
+const SpellController = require('../controllers/SpellController');
+
 
 module.exports = () => {
     const router = express.Router();
-    router.get('/', async (req, res) => {
-        try {
-            const response = await axios.get(process.env.EXTERNAL_DND_API + '/spells');
-        
-            res.json(response.data);
-          } catch (err) {
-            console.error('Error fetching spells:', err);
-            res.status(500).json({ message: 'Error fetching spells from the external API.' });
-          }
-    })
+    // Repositories
+    const dnd5eClient = new Dnd5eClient();
+    const spellRepository = new SpellRepository();
 
-    
+    // Services
+    const spellService = new SpellService(spellRepository, dnd5eClient);
+
+    // Controller
+    const spellController = new SpellController(spellService)
+
+    router.get('/', (req, res) => spellController.getAll(req, res));
+
     return router;
 }
