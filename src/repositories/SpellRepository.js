4cb2b3f2ba15f775f250606dd2ddb920e4aad547
Patrik Dobos <d.patrik0610@gmail.com>
2024-11-27 23:51:29 +0100

    crud for spells

--- src/repositories/SpellRepository.js
+++ src/repositories/SpellRepository.js
@@ -6,10 +6,25 @@ class SpellRepository {
     async getAll() {
       return this.collection.find().toArray();
     }
+    
+    async getByIndex(spellIndex) {
+      return await this.db.findOne({ index: spellIndex });
+    }
   
     async create(spellData) {
       const result = await this.collection.insertOne(spellData);
       return result.ops[0];
+    }
+
+    async deleteOne(spellIndex) {
+      return await this.db.deleteOne({ index: spellIndex });
+    }
+
+    async updateOne(spellIndex, updatedData) {
+      return await this.db.updateOne(
+        { index: spellIndex },
+        { $set: updatedData }
+      );
     }
   }
   
