e454b863148ac7fc9dd5740d5f59c90dcea4bde9
Patrik Dobos <d.patrik0610@gmail.com>
2024-11-28 23:38:08 +0100

    added validation to spell creation

--- src/routes/spellroutes.js
+++ src/routes/spellroutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const Dnd5eClient = require('../repositories/Dnd5eClient');
 const SpellRepository = require("../repositories/SpellRepository");
+
 const SpellService = require('../services/SpellService');
 const SpellController = require('../controllers/SpellController');
 
+const validateRequest = require('../middlewares/ValidateRequest');
+const spellValidator = require('../validators/SpellValidator');
 
 module.exports = (db) => {
     const router = express.Router();
@@ -15,13 +18,13 @@ module.exports = (db) => {
     const spellService = new SpellService(spellRepository, dnd5eClient);
 
     // Controller
-    const spellController = new SpellController(spellService)
+    const spellController = new SpellController(spellService);
 
     router.get('/', (req, res) => spellController.getAll(req, res));
     router.get('/customspells', (req, res) => spellController.getAllCustom(req, res));
     router.get('/:index', (req, res) => spellController.getByIndex(req, res));
 
-    router.post('/', (req, res) => spellController.create(req, res));
+    router.post('/create', validateRequest(spellValidator), (req, res) => spellController.create(req, res));
 
     router.put('/update/:index', (req, res) => spellController.update(req, res));
     router.delete('/delete/:index', (req, res) => spellController.delete(req, res));
