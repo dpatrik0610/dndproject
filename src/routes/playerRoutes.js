94c3b40a2a9582d12edbacb09a8a24ac4ce2b6d2
dpatrik0610 <d.patrik0610@gmail.com>
2024-12-25 11:39:13 +0100

    created routes for player

--- src/routes/playerRoutes.js
+++ src/routes/playerRoutes.js
@@ -2,16 +2,26 @@ const express = require('express');
 const PlayerController = require('../controllers/PlayerController');
 const PlayerService = require('../services/PlayerService');
 const PlayerRepository = require('../repositories/PlayerRepository');
-
+const CurrencyManager = require('../models/Player/PlayerCurrencyManager')
 module.exports = (db) => {
   const router = express.Router();
 
   const playerRepository = new PlayerRepository(db.collection("Players"));
-  const playerService = new PlayerService(playerRepository);
+  const currencyManager = new CurrencyManager();
+  const playerService = new PlayerService(playerRepository, currencyManager);
   const playerController = new PlayerController(playerService);
 
-  router.get('/', (req, res) => playerController.getAll(req, res));
-  router.post('/', (req, res) => playerController.create(req, res));
-
+  router.get('/players', (req, res) => playerController.getPlayers(req, res));
+  
+  router.get('/player/:playerId', (req, res) => playerController.getPlayerById(req, res));
+  router.post('/player', (req, res) => playerController.createPlayer(req, res));
+  router.put('/player/:playerId', (req, res) => playerController.updatePlayer(req, res));
+  router.delete('/player/:playerId', (req, res) => playerController.deletePlayer(req, res));
+  
+  router.get('/player/:playerId/currency', (req, res) => playerController.getTotalCurrency(req, res));
+  router.post('/player/:playerId/currency/add', (req, res) => playerController.addCurrency(req, res));
+  router.delete('/player/:playerId/currency/remove', (req, res) => playerController.removeCurrency(req, res));
+  router.post('/player/:playerId/currency/transfer', (req, res) => playerController.transferCurrency(req, res));
+  
   return router;
 };
