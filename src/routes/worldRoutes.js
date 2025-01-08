0bbc6c6d12666cb721f9bb799ca5f4ef2987beee
Dobos Patrik <d.patrik0610@gmail.com>
2025-01-08 01:32:18 +0100

    using validator in worldRoutes.

--- src/routes/worldRoutes.js
+++ src/routes/worldRoutes.js
@@ -3,6 +3,8 @@ const WorldService = require('../services/WorldService');
 const WorldRepository = require('../repositories/WorldRepository');
 const { logTemplates: logger } = require('../utils/logTemplates');
 const WorldController = require('../controllers/WorldController');
+const validateRequest = require('../middlewares/ValidateRequest');
+const worldValidator = require('../validators/worldValidator');
 
 module.exports = (db) => {
   const router = express.Router();
@@ -16,17 +18,17 @@ module.exports = (db) => {
 
     // World Operations
     router.get('/:worldId', (req, res) => worldController.getById(req, res));
-    router.post('/create', (req, res) => worldController.create(req, res));
-    router.put('/:worldId', (req, res) => worldController.update(req, res));
+    router.post('/create', validateRequest(worldValidator), (req, res) => worldController.create(req, res));
+    router.put('/:worldId', validateRequest(worldValidator), (req, res) => worldController.update(req, res));
     router.delete('/:worldId', (req, res) => worldController.delete(req, res));
 
     // World Editors
-    router.post('/:worldId/region', (req, res) => worldController.addRegion(req, res));
-    router.post('/:worldId/faction', (req, res) => worldController.addFaction(req, res));
-    router.post('/:worldId/item', (req, res) => worldController.addGlobalItem(req, res));
-    router.put('/:worldId/economy', (req, res) => worldController.updateEconomy(req, res));
-    router.post('/:worldId/event', (req, res) => worldController.addEvent(req, res));
-    router.post('/:worldId/player', (req, res) => worldController.addPlayer(req, res));
+    router.post('/:worldId/region', validateRequest(worldValidator), (req, res) => worldController.addRegion(req, res));
+    router.post('/:worldId/faction', validateRequest(worldValidator), (req, res) => worldController.addFaction(req, res));
+    router.post('/:worldId/item', validateRequest(worldValidator), (req, res) => worldController.addGlobalItem(req, res));
+    router.put('/:worldId/economy', validateRequest(worldValidator), (req, res) => worldController.updateEconomy(req, res));
+    router.post('/:worldId/event', validateRequest(worldValidator), (req, res) => worldController.addEvent(req, res));
+    router.post('/:worldId/player', validateRequest(worldValidator), (req, res) => worldController.addPlayer(req, res));
     
     logger.success('World module loaded.');
   } catch (error) {
