b7dcb235035d044bbc0febb2d139c7fda802c501
Dobos Patrik <d.patrik0610@gmail.com>
2025-01-10 01:50:37 +0100

    updated custom error with statusCode

--- src/utils/LayeredError.js
+++ src/utils/LayeredError.js
@@ -1,12 +1,13 @@
 class LayeredError extends Error {
-    constructor(message, layer) {
+    constructor(message, layer, statusCode = 500) {
       super(message);
       this.layer = layer;
       this.name = 'LayeredError';
+      this.statusCode = statusCode;
     }
   
     toString() {
-      return `[${this.layer}] ${this.message}`;
+      return `[${this.layer}] (${this.statusCode}) ${this.message}`;
     }
 }
 module.exports = LayeredError
